9fa86e99a977478fa781dbf169c32279b6fed0bf
John Doe <johndoe@example.com>
2025-05-19 22:28:15 +0200

    use daisy ui

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-  <!-- Router outlet - displays the view component of the active route -->
-  <RouterView />
+
+  <main class="p-4 bg-base-200 h-[100vh] xl:max-w-[1200px] mx-auto">
+
+    <!-- Router outlet - displays the view component of the active route -->
+    <RouterView />
+
+  </main>
 </template>
 
 <style scoped>
--- src/components/Message.vue
+++ src/components/Message.vue
@@ -8,16 +8,22 @@ defineProps({
 </script>
 
 <template>
-  <div v-bind:class="message.user === currentUser?.id ? 'user-message' : ''">
-    {{ message.text }}
-    <span>
-      {{ message.created }}
-    </span>
+ <li class="chat" :class="{ 'chat-end': message.user === currentUser?.id, 'chat-start': message.user !== currentUser?.id }">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img
+          alt="User Avatar"
+          :src="'https://jet-escape.pockethost.io/api/files/users/' + message.user + '/' + message.expand?.user?.avatar"   />
+    </div>
   </div>
+  <div class="chat-header">
+    {{ message.user ?? 'anonymous' }}
+    <time class="text-xs opacity-50">{{ message.created }}</time>
+  </div>
+  <div class="chat-bubble">{{message.text}}</div>
+ </li>
 </template>
 
 <style>
-.user-message {
-  background: aqua;
-}
+
 </style>
